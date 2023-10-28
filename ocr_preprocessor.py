2398456851ee83cc3d7f2ce7de47133cdd48d0a7
tarek12309 <37149475+tarek12309@users.noreply.github.com>
2023-10-28 17:02:33 +0300

    1111111

--- ocr_preprocessor.py
+++ ocr_preprocessor.py
@@ -12,17 +12,20 @@ class OcrPreProcessor:
     ARCHIVE_FOLDER_PATH = "/home/ubuntu/mobileForcesPublic/archive/ocr_archive"
     OCR_FOLDER_ID = None
 
-    def __init__(self, service, google_drive, ocr_folder_id: str):
-        self.service = service
+    def __init__(self, google_drive, ocr_folder_id: str):
         self.google_drive = google_drive
         self.OCR_FOLDER_ID = ocr_folder_id
         self.existing_files = dict()
         self.excel_file_handler = FileHandler(google_drive, self.VALID_FILE_EXTENSIONS)
 
-    def _generate_df_from_ocr_file(self, folder_path, file_name: str, file_id: str):
+    def _generate_df_from_ocr_file(self, folder_path, file_name: str, file_id: str, archived_file_id):
         tmp_file_path = f"{folder_path}{file_name}"
         file = self.google_drive.CreateFile({'id': file_id})
         file.GetContentFile(tmp_file_path)  # Download file as 'example.xlsx'.
+        file.delete()
+        archived_file = self.google_drive.CreateFile({'parent': [{'parent': archived_file_id}]})
+        archived_file.SetContentFile(tmp_file_path)
+        archived_file.Upload()
         ocr_df = pd.read_excel(tmp_file_path)
         return ocr_df
 
@@ -36,14 +39,10 @@ class OcrPreProcessor:
             for title, new_file in new_added_files.items():
                 print(new_file)
                 logging.info(f"Execute logic on: {title}, {new_file['id']}")
-                ocr_df = self._generate_df_from_ocr_file(self.OCR_FOLDER_PATH, title, new_file['id'])
-                previous_parents = ",".join(new_file['parents'][0].get("id"))
-                file = self.service.files().update(
-                    fileId=new_file['id'],
-                    addParents="1MpRnelL2E8Fmxh3Kctp9sfc8p608u2-p",
-                    removeParents=previous_parents,
-                    fields='id, parents'
-                ).execute()
+                ocr_df = self._generate_df_from_ocr_file(self.OCR_FOLDER_PATH, title, new_file['id'], new_file['parents'][0].get("id"))
+
+
+
                 preprocess_df = self.preprocess_file(ocr_df)
                 # TODO: upload preprocess_df as xls to the relavant folder
                 print(preprocess_df)
